f7e06ef393e733f442c3a054caf8086a55a1e6b5
sahil_sagwekar <sahilss2652@gmail.com>
2023-04-12 12:27:49 +0530

    improved main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import re
-# from bs4.diagnose import diagnose
 import requests
-from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
-
-file = open('data1.csv', 'a', encoding='windows-1252')
-file.write("rating,header,customer_country,date,review,Aircraft,Type of Traveller,Seat Type,Route,Date Flown,Seat Comfort,Cabin Staff Service,Food & Beverages,Inflight Entertainment,Ground Service,Wifi & Connectivity,Value For Money,Recommended\n")
+from bs4 import BeautifulSoup, SoupStrainer
 
 URL = "https://www.airlinequality.com/airline-reviews/british-airways/page/page_no/?sortby=post_date%3ADesc&pagesize=100"
 headers = {
@@ -13,60 +8,66 @@ headers = {
     "Accept-Language": "en-US,en;q=0.9"
 }
 
-for i in range(1, 37):
+filename = input("Save data as (filename.extension): ")
+while not filename:
+    print("Invalid filename.")
+    filename = input("Save data as (filename.extension): ")
 
-    response = requests.get(url=URL.replace('page_no', str(i)), headers=headers)
+with open(filename, 'w') as file:
+    file.write("rating,header,customer_country,date,review,Aircraft,Type of Traveller,Seat Type,Route,Date Flown,Seat Comfort,Cabin Staff Service,Food & Beverages,Inflight Entertainment,Ground Service,Wifi & Connectivity,Value For Money,Recommended\n")
 
-    articles = SoupStrainer("article", class_=re.compile("list-item"))
-    soup = BeautifulSoup(response.text, 'html.parser', parse_only=articles)
-    # print(soup.prettify())
-    articles = soup.find_all('article')
+    for i in range(1, 37):
+        response = requests.get(url=URL.replace('page_no', str(i)), headers=headers)
+        articles = SoupStrainer("article", class_=re.compile("list-item"))
+        soup = BeautifulSoup(response.text, 'html.parser', parse_only=articles)
+        articles = soup.find_all('article')
 
-    for article in articles:
-        rating = article.find('div', class_="rating-10").get_text().strip().split("/")[0]
-        header = article.find('h2', class_="text_header").string
-        try:
-            cust_details1 = article.find('h3', class_="text_sub_header").get_text().strip().split("(")
-            date = cust_details1[1].split(") ")[1]
-            customer_country = cust_details1[1].split(") ")[0]
-        except IndexError:
-            customer_country = None
-            date = None
-        try:
-            review = article.find('div', class_="text_content").get_text().split(" |")[1].strip()
-        except IndexError:
-            review = article.find('div', class_="text_content").get_text().strip()
+        for article in articles:
+            rating = article.find('div', class_="rating-10").get_text().strip().split("/")[0]
+            header = article.find('h2', class_="text_header").string.replace(",", "")
 
-        my_dict = {
-            "rating": rating,
-            "header": header,
-            "customer_country": customer_country,
-            "date": date,
-            "review": '"'+review+'"',
-            "review": None,
-            "Aircraft": None,
-            "Type Of Traveller": None,
-            "Seat Type": None,
-            "Route": None,
-            "Date Flown": None,
-            "Seat Comfort": None,
-            "Cabin Staff Service": None,
-            "Food & Beverages": None,
-            "Inflight Entertainment": None,
-            "Ground Service": None,
-            "Wifi & Connectivity": None,
-            "Value For Money": None,
-            "Recommended": None
-        }
+            try:
+                cust_details1 = article.find('h3', class_="text_sub_header").get_text().strip().split("(")
+                customer_country = cust_details1[1].split(") ")[0]
+                date = cust_details1[1].split(") ")[1]
+            except (AttributeError, IndexError):
+                customer_country, date = '', ''
 
-        article = BeautifulSoup(str(article), 'html.parser')
+            try:
+                review = article.find('div', class_="text_content").get_text().split(" |")[1].strip().replace(",", "")
+            except IndexError:
+                review = article.find('div', class_="text_content").get_text().strip().replace(",", "")
 
-        for i in article.find_all('td', 'review-value'):
-            my_dict[i.previousSibling.get_text()] = i.get_text()
+            my_dict = {
+                "rating": rating,
+                "header": header,
+                "customer_country": customer_country,
+                "date": date,
+                "review": '"'+review+'"',
+                "Aircraft": '',
+                "Type Of Traveller": '',
+                "Seat Type": '',
+                "Route": '',
+                "Date Flown": '',
+                "Seat Comfort": '',
+                "Cabin Staff Service": '',
+                "Food & Beverages": '',
+                "Inflight Entertainment": '',
+                "Ground Service" : '',
+                "Wifi & Connectivity" : '',
+                "Value For Money" : '',
+                "Recommended": ''
+                }
 
-        for j in article.find_all('td', 'review-rating-stars'):
-            my_dict[j.parent.find('td', 'review-rating-header').get_text()] = str(len(j.parent.find_all('span', class_="fill")))
+            article = BeautifulSoup(str(article), 'html.parser')
 
-        print(",".join(str(value) for value in my_dict.values()), file=file)
+            for i in article.find_all('td', 'review-value'):
+                my_dict[i.previousSibling.get_text()] = i.get_text()
 
-file.close()
+            for j in article.find_all('td', 'review-rating-stars'):
+                my_dict[j.parent.find('td', 'review-rating-header').get_text()] = str(len(j.parent.find_all('span', class_="fill")))
+
+            try:
+                print(",".join(str(value) for value in my_dict.values()), file=file)
+            except UnicodeEncodeError:
+                pass
